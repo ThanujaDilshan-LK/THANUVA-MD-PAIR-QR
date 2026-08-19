c12b96caf1b7cc4b6938b15e9e68cc5f050922e1
ThanujaDilshan-LK <thanujadilsahh2011@gmail.com>
2026-08-19 14:07:36 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -2,8 +2,8 @@ import * as mega from 'megajs';
 
 // Mega authentication credentials
 const auth = {
-    email: 'abc@gmail.com', // Replace with your Mega email
-    password: 'abc@1234!', // Replace with your Mega password
+    email: 'thanujadilsahh2011@gmail.com', // Replace with your Mega email
+    password: 'n+QK*z58#mZg8dc', // Replace with your Mega password
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
